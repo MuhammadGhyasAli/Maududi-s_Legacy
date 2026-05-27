d5fb934cdd013905fa2e83d67bad6f699bee72bb
Syed Ghyas <sgnaqvi05@gmail.com>
2026-05-27 18:05:23 +0500

    Remove hardcoded MongoDB URI, read only from env

--- src/lib/mongodb.ts
+++ src/lib/mongodb.ts
@@ -1,6 +1,6 @@
 import { MongoClient, type Db } from 'mongodb';
 
-const MONGODB_URL = process.env.MONGODB_URL || 'mongodb+srv://syedali:yxUXgGk5LVUb36Cb@cluster0.ne39ibs.mongodb.net/?appName=Cluster0';
+const MONGODB_URL = process.env.MONGODB_URL;
 const MONGODB_DB_NAME = process.env.MONGODB_DB_NAME || 'maududi_legacy';
 
 let client: MongoClient | null = null;
